53db3dae3a6fdcd7826f63cbce0c5140b3523f38
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-08-13 14:08:58 -0700

    asdasf

--- contrib/jenkins/configuration/init.groovy.d/001-setup-slave-credential.groovy
+++ contrib/jenkins/configuration/init.groovy.d/001-setup-slave-credential.groovy
@@ -2,6 +2,7 @@ import com.cloudbees.plugins.credentials.impl.*;
 import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
 import org.jenkinsci.plugins.plaincredentials.impl.*;
+import jenkins.model.Jenkins
 import hudson.model.*
 import hudson.util.Secret;
 
@@ -16,13 +17,14 @@ def runOrDie(def command, String errorMessage){
 }
 
 Thread.start {
-    String openshiftSecretName = 'jenkins-slave-user'
-    String username=runOrDie(['sh', '-c', "oc get secret/${openshiftSecretName} --template={{.data.username}} | base64 --decode"], "'secret/${openshiftSecretName}' was NOT nound")
+    String openshiftPodNamespace=new File('/var/run/pod/namespace').getText('UTF-8').trim()
+    String openshiftSecretName = new File('/var/run/secrets/jenkins-slave-user/metadata.name').getText('UTF-8').trim()
+    String username = new File('/var/run/secrets/jenkins-slave-user/username').getText('UTF-8').trim()
 
     User u = User.get(username)
     def apiToken=u.getProperty(jenkins.security.ApiTokenProperty.class)
     println "\'${u.getId()}\' API token:${apiToken.getApiTokenInsecure()}"
-    ['oc','patch', "secret/${openshiftSecretName}", '-p', '{"stringData": {"password": "'+apiToken.getApiTokenInsecure()+'"}}', '-n', 'csnr-devops-lab-tools'].execute().waitFor()
+    ['oc','patch', "secret/${openshiftSecretName}", '-p', '{"stringData": {"password": "'+apiToken.getApiTokenInsecure()+'"}}', '-n', openshiftPodNamespace].execute().waitFor()
 
     
     Jenkins.instance.getAuthorizationStrategy().add(hudson.slaves.SlaveComputer.CREATE, username)
