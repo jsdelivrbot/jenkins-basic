81ae60c9052b3fa99b0c5a87ba764109a53a857d
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-08-13 21:23:23 -0700

    asd

--- instance/contrib/jenkins/configuration/init.groovy.d/102-job-script-approvals.groovy
+++ instance/contrib/jenkins/configuration/init.groovy.d/102-job-script-approvals.groovy
@@ -4,9 +4,16 @@ import org.jenkinsci.plugins.scriptsecurity.scripts.languages.*
 
 
 Thread.start {
-    Thread.sleep(1000)
+    java.time.Instant startTime = java.time.Instant.now()
+
     final ScriptApproval sa = ScriptApproval.get();
-    def job = Jenkins.instance.getItem('ON_GH_EVENT');
+    def job = null
+    while (job == null) {
+        if (java.time.Duration.between(startTime, java.time.Instant.now()).getSeconds() > 60) throw new RuntimeException('Timeout!')
+        Thread.sleep(1000);
+        job = Jenkins.instance.getItem('ON_GH_EVENT')
+    }
+
     for (hudson.tasks.Builder builder:job.getBuilders()){
         if (builder instanceof hudson.plugins.groovy.SystemGroovy){
             def builderSource = builder.getSource()
