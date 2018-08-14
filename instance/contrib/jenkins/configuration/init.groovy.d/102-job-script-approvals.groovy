66518f2214b803c653a2494a4b91373e089c7c1c
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-08-14 09:05:15 -0700

    ads

--- instance/contrib/jenkins/configuration/init.groovy.d/102-job-script-approvals.groovy
+++ instance/contrib/jenkins/configuration/init.groovy.d/102-job-script-approvals.groovy
@@ -6,25 +6,24 @@ import org.jenkinsci.plugins.scriptsecurity.scripts.languages.*
 //Thread.start {
     java.time.Instant startTime = java.time.Instant.now()
     def job = null
+    ['ON_GH_EVENT', 'ON_STARTUP'].each { jobName ->
+        //wait for job to load
+        while (job == null) {
+            if (java.time.Duration.between(startTime, java.time.Instant.now()).getSeconds() > 60) throw new RuntimeException('Timeout!')
+            //println 'Waiting for ON_GH_EVENT to load'
+            Thread.sleep(1000);
+            job = Jenkins.instance.getItemByFullName(jobName)
+        }
 
-    //wait for job to load
-    while (job == null) {
-        
-        if (java.time.Duration.between(startTime, java.time.Instant.now()).getSeconds() > 60) throw new RuntimeException('Timeout!')
-        //println 'Waiting for ON_GH_EVENT to load'
-        Thread.sleep(1000);
-        job = Jenkins.instance.getItemByFullName('ON_GH_EVENT')
-        //println "ON_GH_EVENT:${job}"
-    }
-
-    final ScriptApproval sa = ScriptApproval.get();
-    for (hudson.tasks.Builder builder:job.getBuilders()){
-        if (builder instanceof hudson.plugins.groovy.SystemGroovy){
-            def builderSource = builder.getSource()
-            if (builderSource instanceof hudson.plugins.groovy.StringSystemScriptSource){
-                String scriptSource = builderSource.getScript().getScript()
-                ScriptApproval.PendingScript s = new ScriptApproval.PendingScript(scriptSource, GroovyLanguage.get(), ApprovalContext.create())
-                sa.approveScript(s.getHash())
+        final ScriptApproval sa = ScriptApproval.get();
+        for (hudson.tasks.Builder builder:job.getBuilders()){
+            if (builder instanceof hudson.plugins.groovy.SystemGroovy){
+                def builderSource = builder.getSource()
+                if (builderSource instanceof hudson.plugins.groovy.StringSystemScriptSource){
+                    String scriptSource = builderSource.getScript().getScript()
+                    ScriptApproval.PendingScript s = new ScriptApproval.PendingScript(scriptSource, GroovyLanguage.get(), ApprovalContext.create())
+                    sa.approveScript(s.getHash())
+                }
             }
         }
     }
