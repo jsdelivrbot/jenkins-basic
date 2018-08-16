88d75633e356465cb5a0312da7bfa1a1be9fd205
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-08-15 23:02:41 -0700

    asdasd

--- instance/contrib/jenkins/configuration/init.groovy.d/099-configure-jobs.groovy
+++ instance/contrib/jenkins/configuration/init.groovy.d/099-configure-jobs.groovy
@@ -1,3 +1,5 @@
+class ConfigureJobs009 extends Script {
+
 static Map exec(List args, Appendable stdout=null, Appendable stderr=null, Closure stdin=null){
     ProcessBuilder builder = new ProcessBuilder(args)
     def proc = builder.start()
@@ -24,20 +26,25 @@ static Map exec(List args, Appendable stdout=null, Appendable stderr=null, Closu
     return ret
 }
 
-String secretToken = UUID.randomUUID()
-Map ocGetSecretToken = exec(['sh', '-c', "set -x; oc get \"secret/\$(cat /var/run/secrets/github/metadata.name)\" \"--output=jsonpath={.data['generic-hook\\.token']}\" | base64 --decode"])
+def run() {
+    String secretToken = UUID.randomUUID()
+    Map ocGetSecretToken = exec(['sh', '-c', "set -x; oc get \"secret/\$(cat /var/run/secrets/github/metadata.name)\" \"--output=jsonpath={.data['generic-hook\\.token']}\" | base64 --decode"])
 
-if (ocGetSecretToken.status != 0 || ocGetSecretToken.out.toString().trim() == ""){
-    println "Updating/Creating token"
-    exec(['sh', '-c', "oc patch \"secret/\$(cat /var/run/secrets/github/metadata.name)\" -p '{\"stringData\": {\"generic-hook.token\": \"${secretToken}\"}}'" as String])
-}else{
-  	println "Using existing token"
-    secretToken = ocGetSecretToken.out.toString()
+    if (ocGetSecretToken.status != 0 || ocGetSecretToken.out.toString().trim() == ""){
+        println "Updating/Creating token"
+        exec(['sh', '-c', "oc patch \"secret/\$(cat /var/run/secrets/github/metadata.name)\" -p '{\"stringData\": {\"generic-hook.token\": \"${secretToken}\"}}'" as String])
+    }else{
+        println "Using existing token"
+        secretToken = ocGetSecretToken.out.toString()
+    }
+
+
+    def job = Jenkins.instance.getItem('ON_GH_EVENT')
+    job.getTrigger(org.jenkinsci.plugins.gwt.GenericTrigger.class).setToken(secretToken)
+    job.save()
 }
+    static void main(String[] args) {           
+        org.codehaus.groovy.runtime.InvokerHelper.runScript(ConfigureJobs009, args)     
+    }
 
-
-def job = Jenkins.instance.getItem('ON_GH_EVENT')
-job.getTrigger(org.jenkinsci.plugins.gwt.GenericTrigger.class).setToken(secretToken)
-job.save()
-
-return null
+}
