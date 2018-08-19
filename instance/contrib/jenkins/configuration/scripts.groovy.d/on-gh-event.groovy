1d9c7dc242a86b4803eb4d16f564dee1f3b5f072
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-08-18 22:31:20 -0700

    asdasd

--- instance/contrib/jenkins/configuration/scripts.groovy.d/on-gh-event.groovy
+++ instance/contrib/jenkins/configuration/scripts.groovy.d/on-gh-event.groovy
@@ -32,6 +32,8 @@ static Map exec(List args, File workingDirectory=null, Appendable stdout=null, A
 
 String ghPayload = build.buildVariableResolver.resolve("payload")
 String ghEventType = build.buildVariableResolver.resolve("x_github_event")
+String buildNumber = build.getNumber()
+String fullName = build.getProject().getFullName()
 
 //println "ghEventType:"
 //println "${ghEventType}"
@@ -42,20 +44,34 @@ String ghEventType = build.buildVariableResolver.resolve("x_github_event")
 //binding.variables.each{ 
 //  println "${it.key}:${it.value}"
 //}
+File workDir = new File("/tmp/jenkins/on-gh-event/${fullName}/${buildNumber}")
+try{
+    if ("pull_request" == ghEventType){
+        def payload = new JsonSlurper().parseText(ghPayload)
+        if ("closed" == payload.action){
+            File gitWorkDir = workDir
+            def ghRepo=com.cloudbees.jenkins.GitHubRepositoryName.create(payload.repository.clone_url).resolveOne()
+            boolean isFromCollaborator=ghRepo.root.retrieve().asHttpStatusCode(ghRepo.getApiTailUrl("collaborators/${payload.pull_request.user.login}")) == 204
+            String cloneUrl = payload.repository.clone_url
+            String sourceBranch = isFromCollaborator?"refs/pull/${payload.number}/head":"refs/heads/${payload.pull_request.base.ref}"
+            println "Is Collaborator:${isFromCollaborator} (${payload.pull_request.user.login})"
+            println "Clone Url:${cloneUrl}"
+            println "Checkout Branch:${sourceBranch}"
 
-if ("pull_request" == ghEventType){
-    def payload = new JsonSlurper().parseText(ghPayload)
-    if ("closed" == payload.action){
-        File gitWorkDir = new File("tmp")
-        println "git root:${gitWorkDir.getAbsolutePath()}"
+            println exec(['mkdir', '-p', gitWorkDir.getAbsolutePath()])
+            println exec(['rm', '-rf', gitWorkDir.getAbsolutePath()])
+            println exec(['git', 'init', gitWorkDir.getAbsolutePath()])
+            println exec(['git', 'remote', 'add', 'origin', payload.repository.clone_url], gitWorkDir)
+            println exec(['git', 'fetch', '--no-tags', payload.repository.clone_url, "+${sourceBranch}:PR-${payload.number}"], gitWorkDir)
+            println exec(['git', 'checkout', "PR-${payload.number}"] , gitWorkDir)
 
-        println exec(['rm', '-rf', gitWorkDir.getAbsolutePath()])
-        println exec(['git', 'init', gitWorkDir.getAbsolutePath()])
-        println exec(['git', 'remote', 'add', 'origin', payload.repository.clone_url], gitWorkDir)
-        println exec(['git', 'fetch', '--no-tags', payload.repository.clone_url, "+refs/pull/${payload.number}/head:PR-${payload.number}"], gitWorkDir)
-        println exec(['git', 'checkout', "PR-${payload.number}"] , gitWorkDir)
-
-        ///Users/cvarjao/Documents/GitHub/mds/
-        exec(['sh', '-c', "pipeline/gradlew --no-build-cache --console=plain --no-daemon -b pipeline/build.gradle cd-clean -Pargs.--config=pipeline/config.groovy -Pargs.--pr=${payload.number}"] , gitWorkDir, binding.variables.out)
+            ///Users/cvarjao/Documents/GitHub/mds/
+            exec(['sh', '-c', "pipeline/gradlew --no-build-cache --console=plain --no-daemon -b pipeline/build.gradle cd-clean -Pargs.--config=pipeline/config.groovy -Pargs.--pr=${payload.number}"] , gitWorkDir, binding.variables.out)
+        }
     }
+}finally{
+    exec(['rm', '-rf', workDir.getAbsolutePath()])
 }
+
+
+return null;
