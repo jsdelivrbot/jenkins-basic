228f464e0c52ef8e66674ba696a6ca004f96d122
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-08-23 21:28:57 -0700

    asd

--- example/jenkins/openshift/config.groovy
+++ example/jenkins/openshift/config.groovy
@@ -10,6 +10,14 @@ app {
             namespace = app.namespaces.'build'.namespace
             disposable = false
         }
+        'test' {
+            namespace = app.namespaces.'build'.namespace
+            disposable = false
+        }
+        'prod' {
+            namespace = app.namespaces.'build'.namespace
+            disposable = false
+        }
     }
 
     git {
@@ -22,10 +30,11 @@ app {
     build {
         env {
             name = "build"
-            id = "pr-1"
+            id = "pr-${opt.'pr'}"
         }
-        id = "${app.name}"
-        version = "${app.build.env.name}-v1"
+        suffix = "-build-${opt.'pr'}"
+        id = "${app.name}${app.build.suffix}"
+        version = "${app.build.env.name}-v${opt.'pr'}"
         name = "${app.name}" //
 
         namespace = app.namespaces.'build'.namespace
@@ -35,6 +44,7 @@ app {
                     'file':'example/jenkins/openshift/jenkins.bc.json',
                     'params':[
                         'NAME': "${app.build.name}",
+                        'SUFFIX': "${app.build.suffix}",
                         'VERSION': app.build.version,
                         'SOURCE_REPOSITORY_URL': "${app.git.uri}",
                         'SOURCE_REPOSITORY_REF': "${app.git.ref}"
@@ -48,18 +58,25 @@ app {
             name = vars.deployment.env.name // env-name
             id = vars.deployment.env.id
         }
-        id = "${app.name}" // app (unique name across all deployments int he namespace)
-        version = "${app.deployment.env.name}-v1" //app-version  and tag
+        suffix = "${vars.deployment.suffix}" // app (unique name across all deployments int he namespace)
+        version = "${app.deployment.env.name}-v${opt.'pr'}" //app-version  and tag
         name = "${app.name}" //app-name   (same name accross all deployments)
+        id = "${app.deployment.name}${app.deployment.suffix}" // app (unique name across all deployments int he namespace)
 
         namespace = "${vars.deployment.namespace}"
         timeoutInSeconds = 60*20 // 20 minutes
+        host = "${app.deployment.id}-${app.deployment.namespace}.pathfinder.gov.bc.ca"
+
         templates = [
                 [
                     'file':'example/jenkins/openshift/jenkins.dc.json',
                     'params':[
-                        'NAME':app.deployment.name,
-                        'VERSION': app.deployment.version
+                        'NAME': "${app.deployment.name}",
+                        'SUFFIX': "${app.deployment.suffix}",
+                        'VERSION': app.deployment.version,
+                        'ROUTE_HOST': app.deployment.host,
+                        'ENV_NAME':app.deployment.env.name,
+                        'ENV_ID':app.deployment.env.id
                     ]
                 ]
         ]
@@ -72,9 +89,36 @@ environments {
             deployment {
                 env {
                     name ="dev"
-                    id = "pr-1"
+                    id = "pr-${opt.'pr'}"
                 }
-                id = "${app.name}-dev"
+                suffix = "-dev-${opt.'pr'}"
+                name = "${app.name}"
+                namespace = app.namespaces[env.name].namespace
+            }
+        }
+    }
+    'test' {
+        vars {
+            deployment {
+                env {
+                    name ="test"
+                    id = "pr-${opt.'pr'}"
+                }
+                suffix = '-test'
+                name = "${app.name}"
+                namespace = app.namespaces[env.name].namespace
+            }
+        }
+    }
+    'prod' {
+        vars {
+            deployment {
+                env {
+                    name ="prod"
+                    id = "pr-${opt.'pr'}"
+                }
+                suffix = ''
+                id = "${app.name}${vars.deployment.suffix}"
                 name = "${app.name}"
                 namespace = app.namespaces[env.name].namespace
             }
