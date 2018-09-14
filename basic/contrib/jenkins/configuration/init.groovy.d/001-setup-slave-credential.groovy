a6905a1a2dfb8ba4f6ea215080877c87cd4c837d
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-09-13 23:04:59 -0700

    Latest Jenkins Token Creation

--- basic/contrib/jenkins/configuration/init.groovy.d/001-setup-slave-credential.groovy
+++ basic/contrib/jenkins/configuration/init.groovy.d/001-setup-slave-credential.groovy
@@ -14,9 +14,24 @@ String username = new File('/var/run/secrets/jenkins-slave-user/username').getTe
 
 User u = User.get(username)
 def apiToken=u.getProperty(jenkins.security.ApiTokenProperty.class)
-println "\'${u.getId()}\' API token:${apiToken.getApiTokenInsecure()}"
-['oc','patch', "secret/${openshiftSecretName}", '-p', '{"stringData": {"password": "'+apiToken.getApiTokenInsecure()+'"}}', '-n', openshiftPodNamespace].execute().waitFor()
 
+//BEFORE 2.129+: https://jenkins.io/blog/2018/07/02/new-api-token-system/
+//println "\'${u.getId()}\' API token:${apiToken.getApiTokenInsecure()}"
+//['oc','patch', "secret/${openshiftSecretName}", '-p', '{"stringData": {"password": "'+apiToken.getApiTokenInsecure()+'"}}', '-n', openshiftPodNamespace].execute().waitFor()
+
+//AFTER 2.129+: https://jenkins.io/blog/2018/07/02/new-api-token-system/
+
+//Revoke all existing tokens
+for (def token:apiToken.getTokenList()){
+    def revoked = apiToken.tokenStore.revokeToken(token.uuid)
+    if(revoked != null){
+        p.tokenStats.removeId(revoked.getUuid());
+    }
+}
+def newToken= apiToken.tokenStore.generateNewToken('swarm')
+['oc','patch', "secret/${openshiftSecretName}", '-p', '{"stringData": {"password": "'+newToken.plainValue+'"}}', '-n', openshiftPodNamespace].execute().waitFor()
+println "\'${u.getId()}\' API token:${newToken.plainValue}"
+u.save()
 
 Jenkins.instance.getAuthorizationStrategy().add(hudson.slaves.SlaveComputer.CREATE, username)
 Jenkins.instance.getAuthorizationStrategy().add(hudson.slaves.SlaveComputer.CONNECT, username)
